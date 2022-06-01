1f280a7c65b5daa206561c30bf7239bc11096c4d
Fotis Alexandridis <eleft@protonmail.com>
2022-06-01 22:44:06 +0300

    Branch to convert a single captured ros2bag to a pcd file to use in the calibration process

---
+++ src/ros2bag_to_pcd.cpp
@@ -0,0 +1,15 @@
+#include "utils.hpp"
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/common/io.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+int main(/*int argc, char* argv[]*/) {
+    // get pcl2
+    sensor_msgs::msg::PointCloud2 pclMsg = get_pcl_from_rosbag();
+
+    // save PCD
+    pcl::io::savePCDFile("lidar.pcd", pclMsg); 
+    return 0;
+}
